c934e99137ad161f363c60e2587ed6627aa2c98a
Amar G Nath <ambadypradeepcoc@gmail.com>
2024-02-10 21:40:10 +0530

    final commit

--- src/components/Syllabus.jsx
+++ src/components/Syllabus.jsx
@@ -6,18 +6,22 @@ function Syllabus({ course }) {
   console.log(syllabus);
 
   return (
-    <div className="w-full max-h-max">
-      <p className="text-2xl font-bold py-5">Syllabus</p>
-      <div className="flex justify-center items-center w-full gap-5 overflow-x-scroll syllabus-container">
-        {syllabus.map((course, key) => (
-          <div key={key} className="flex flex-col bg-purple-200 px-20 py-5 rounded-xl gap-2">
-            <p className="font-bold text-justify text-lg">week {course.week}</p>
-            <p className="text-justify text-lg">{course.topic}</p>
-            <p className="text-justify text-md">{course.content}</p>
+    <div className="w-full">
+    <p className="text-2xl font-bold py-5">Syllabus</p>
+    <div className="flex flex-wrap justify-center items-center gap-5 overflow-x-scroll syllabus-container">
+      {syllabus.map((course, key) => (
+        <div key={key} className="flex flex-1  bg-purple-200 px-10 sm:px-20 py-5 rounded-xl gap-2">
+          <div className="flex-col "
+          style={{minHeight: '200px'}}>
+
+          <p className="font-bold text-lg">Week {course.week}</p>
+          <p className="text-lg">{course.topic}</p>
+          <p className="text-md">{course.content}</p>
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
     </div>
+  </div>
   );
 }
 
